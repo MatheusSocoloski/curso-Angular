b63d36e7b1eaa9ec46b08d297548307bc8e70dfa
MatheusSocoloski <62072839+MatheusSocoloski@users.noreply.github.com>
2023-07-14 11:56:42 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessageComponent } from './components/messages/messages.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    NewMomentComponent,
+    MomentFormComponent,
+
+    MessageComponent,
+     MomentComponent,
+     EditMomentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moments';
+
+import { Router } from '@angular/router';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+  btnText = 'Compartilhar!';
+
+  constructor(private momentService: MomentService, 
+    private messagesService: MessagesService,
+    private router: Router){}
+
+  async createHandler(moment: Moment){
+    const formData = new FormData();
+
+
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if(moment.image){
+      formData.append('image', moment.image);
+    }
+
+    // todo
+
+     await this.momentService.createMoment(formData).subscribe;
+
+    this.messagesService.add('momento adicionado com sucesso!');
+
+    this.router.navigate(['/']);
+  }
+
+}
